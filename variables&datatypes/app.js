909eeb3d538c2b172245df989285129a883bc413
AngelikaGlaser <angelikaglaser2001@gmail.com>
2022-09-14 10:57:25 +0300

    Text output and comments

---
+++ variables&datatypes/app.js
@@ -0,0 +1,16 @@
+/**
+ * documentation
+ * comment
+ * style
+ */
+//this is simple comment on 1 row
+
+console.log("Hello, world!")
+console.log("Second row")
+console.log("third row")
+
+/*
+console.log("Hello, world!")
+console.log("Hello, world!")
+console.log("Hello, world!")
+*/
