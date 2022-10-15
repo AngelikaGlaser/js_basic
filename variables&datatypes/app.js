5d15fcc16deaae99623c0b51b07ff85e11618572
AngelikaGlaser <angelikaglaser2001@gmail.com>
2022-10-15 19:00:51 +0300

    3.2 Janesevanemate Mure

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -1,7 +1,24 @@
 //3.1 Aratus
+/*
 let MituKorda = prompt("Mitu korda aratus heliseb? ")
 let kord = 1
 while(kord <= MituKorda){
     console.log(`Touse ja Sara - ${kord}. kord`)
     kord++
 }
+*/
+//3.2 Janesevanemate mure
+let ring = 1
+let RingideArv =Number(prompt('Sisesta ringide arv: '))
+let porgandidKokku = 0
+
+while(ring <= RingideArv){
+    console.log(`${ring}. ring`)
+    if (ring % 2 == 0){
+        //console.log(`Saab ${ring} porgandit`)
+        porgandidKokku += ring
+        //console.log(`Kokku saanud ${porgandidKokku} porgandit`)
+    }
+    ring ++
+}
+console.log((`Kokku sai ${porgandidKokku} porgandit`))
